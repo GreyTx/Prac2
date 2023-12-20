188ffb3ce08c1b5a7271e67e1428bd36b5b31dcc
Grey123e <g.donchyk@gmail.com>
2023-12-20 14:11:42 -0600

    Removed Main

--- src/OOP/Main2.java
+++ src/OOP/Main2.java
@@ -3,15 +3,7 @@ package OOP;
 
 public class Main2 {
 
-    public static void main(String[] args) {
 
-
-        System.out.println("Hellow world!");
-        System.out.println("Hellow world!");
-
-
-
-    }
 
 
 
