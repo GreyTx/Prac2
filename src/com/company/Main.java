bf30e17f6c1656a2c8b1dfd7856c95ad5e373f29
Grey123e <g.donchyk@gmail.com>
2024-01-03 20:02:32 -0600

    revert back
    this reverts commit 75c29dd174201125b59aa720d2dc426157ce024c.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Main {
 
-    public static void testCase4321(String[] args) {
+    public static void main(String[] args) {
 
         System.out.println("Hello world!");
         System.out.println("Hello world!");
@@ -10,6 +10,11 @@ public class Main {
         System.out.println("Hello world!");
 
 
+        String name = "Grey";
+        String food = "apple";
+
+        System.out.println("Dummy code");
+        System.out.println("Hello world!");
 
     }
 public static void testCase123() {
