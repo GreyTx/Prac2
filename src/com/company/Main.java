4b35600d491fcc0c4fed7c11cfa7690d16c5fd00
GreyTx <82722264+GreyTx@users.noreply.github.com>
2024-01-03 19:31:48 -0600

    Update Main.java
    new test case from github

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,19 +2,7 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-
-
-        String name = "Grey";
-        String food = "apple";
-
-        System.out.println("Dummy code");
-        System.out.println("Hello world!");
-
-    }
+public static void testCase123() {
+//test logic here
+}
 }
